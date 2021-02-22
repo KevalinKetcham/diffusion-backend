70b3699cd379718bd46e6a735cf35eccb53c1f9f
Kevalin Ketcham <kevalin.create@gmail.com>
2021-02-21 19:56:00 -0800

    Add backend dir from central repo

---
+++ backend/User.js
@@ -0,0 +1,16 @@
+let mongoose = require('mongoose');
+
+let UserModel = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+})
+
+module.exports = mongoose.model('User', UserModel)
+
